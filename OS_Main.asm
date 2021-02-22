f21edfe76c3192ab3ecb805440c819844d9a1d75
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2021-02-22 20:07:47 +1300

    Minor tweaks

--- OS_Main.asm
+++ OS_Main.asm
@@ -3,23 +3,23 @@
 mov bp, 0x8000	; Set address far away so the code does not get overrwritten by the stack
 mov sp, bp		; Set stack to top
 
-mov bx, hello_string
+mov bx, HELLO_STRING
 call print
 call print_nl
 
-mov bx, goodbye_string
+mov bx, GOODBYE_STRING
 call print
 call print_nl
 
-jmp $					; Jump to current location - Infinite loop
-
-hello_string:
-	db "Hello World!", 0
-
-goodbye_string:
-	db "Goodbye.", 0
+jmp $			; Jump to current location - Infinite loop
 
 %include "OS_Print.asm"
+
+HELLO_STRING:
+	db "Hello World!", 0
+
+GOODBYE_STRING:
+	db "Goodbye.", 0
 
 times 510-($-$$) db 0	; Fill with 510 zeros minus the size of the code
 dw 0xaa55				; The 511th and 512th byte tells the bios that this is an operating system
--- OS_Print.asm
+++ OS_Print.asm
@@ -1,29 +1,30 @@
 print:
-	pusha				; Rembember old register values so we can set them back
+	pusha				; Rembember old register values on the stack so we can set them back
 
-	.start_print:
+	mov ah, 0x0e		; Set mode to TTY
+
+	start_print:
 
 	mov al, [bx]		; Get the value in pointed to by bx
 
 	cmp al, 0			; Check if 0 then end of string so exit
 	je end_print
 
-	; Print
-	mov ah, 0x0e		; Set mode to TTY
 	int 0x10			; Write character that is in al
 
 	add bx, 1			; Add one to the pointer to move to the next character
-	jmp .start_print	; Go to the start
+	jmp start_print		; Go to the start
 
 	end_print:
 
-	popa				; Set the register to their old values
+	popa				; Set the registers to their old values
 	ret
 
 print_nl:
 	pusha
 
-	mov ah, 0x0e
+	mov ah, 0x0e	; Set mode to TTY
+
 	mov al, 0x0a	; newline
 	int 0x10
 	mov al, 0x0d	; carrage return
